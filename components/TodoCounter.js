0af582436edfdfc62052b4e3f29adedc50e28485
Bless Gomes <blessmgomes@gmail.com>
2026-03-17 00:27:58 -0400

    some minor tweaks

--- components/TodoCounter.js
+++ components/TodoCounter.js
@@ -1,28 +1,21 @@
 class TodoCounter {
-  // todos should be the array of initial todos
-  // selector is the selector for the counter text element
   constructor(todos, selector) {
     this._element = document.querySelector(selector);
-    this._completed = 0;
     this._total = todos.length;
     this._completed = todos.filter((todo) => todo.completed).length;
-  }
-  
-  updateCompleted = (increment) => {
-        this._completed += increment ? 1 : -1;
-        this._updateText();
-  };
+    this._updateText();
+}
 
-  // Call this when a to-do is deleted, or when a to-do is   
-  // created via the form. 
-  updateTotal = (increment) => {
-    // if increment is true, add 1 to this._total. Otherwise, 
-    // subtract 1. In either case, call the method to update the  
-    // text content.  
+  updateCompleted = (increment) => {
+    this._completed += increment ? 1 : -1;
     this._updateText();
   };
 
-  // Call the method to update the text content
+  updateTotal = (increment) => {
+    this._total += increment ? 1 : -1;
+    this._updateText();
+  };
+
   _updateText() {
     this._element.textContent = `Showing ${this._completed} out of ${this._total} completed`;
   }
